8fb730b6d07058f8f03407bcfe29dd6a891a506f
xuwei-k <6b656e6a69@gmail.com>
2012-12-23 04:46:23 +0900

    Scala 2.10.0

--- project/build.scala
+++ project/build.scala
@@ -2,9 +2,9 @@ import sbt._,Keys._
 
 object build extends Build{
   val buildSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.10.0-RC3",
+    scalaVersion := "2.10.0",
     resolvers ++= Seq(
-      "sonatype" at "https://oss.sonatype.org/content/repositories/snapshots"
+      Opts.resolver.sonatypeReleases
     )
   )
 
